a2cf91699c0e96a9cdeddb9d7d07acffbc552b98
Kunal Gandhi <kunal@kunals-macbook-pro.home>
2018-04-03 09:43:43 +0200

    Initial Commit

---
+++ Memory-assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memory-assignment
+//
+//  Created by Kunal Gandhi on 03.04.18.
+//  Copyright © 2018 Kunal Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
